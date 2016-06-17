f73f16e5731d1a6f82f541ff82a32a5c69550973
Varun Somani <vsomani2@illinois.edu>
2016-06-17 11:48:27 -0500

    Commented out .sh

--- development-openrc.sh
+++ development-openrc.sh
@@ -1,8 +1,8 @@
-export OS_USERNAME=vsomani2
+#export OS_USERNAME=vsomani2
 #export OS_PASSWORD=password
-export OS_TENANT_NAME=development
-export PASSWORD_PROMPT=TRUE
-export OS_AUTH_URL=https://roger-openstack.ncsa.illinois.edu
+#export OS_TENANT_NAME=development
+#export PASSWORD_PROMPT=TRUE
+#export OS_AUTH_URL=https://roger-openstack.ncsa.illinois.edu
 # The following lines can be omitted
 #export OS_TENANT_ID=tenantIDString
 #export OS_REGION_NAME=regionName
